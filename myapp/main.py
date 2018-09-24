7bdef11915f78e448ccf5f8eaedf5eef02f21092
Konstantin Palagachev <konstantin.palagachev@gmail.com>
2018-09-25 00:41:37 +0200

    first working bokeh app

--- myapp/main.py
+++ myapp/main.py
@@ -1,38 +1,52 @@
-from bokeh.io import show
-from bokeh.models import ColumnDataSource
-from numpy.random import random
+from os.path import dirname, join
+import pandas as pd
 
-from bokeh.layouts import column, row
+from bokeh.io import curdoc
+from bokeh.layouts import layout, widgetbox
+from bokeh.models import ColumnDataSource, Div
+from bokeh.models.widgets import RangeSlider, MultiSelect, Select
 from bokeh.plotting import figure
-from bokeh.models.widgets import Select, TextInput
-
-def get_data(N):
-    return dict(x=random(size=N), y=random(size=N), r=random(size=N) * 0.03)
-
-COLORS = ["black", "firebrick", "navy", "olive", "goldenrod"]
-
-def modify_doc(doc):
-    source = ColumnDataSource(data=get_data(200))
-
-    p = figure(tools="", toolbar_location=None)
-    r = p.circle(x='x', y='y', radius='r', source=source,
-                 color="navy", alpha=0.6, line_color="white")
+from bokeh.transform import factor_cmap
 
 
-    select = Select(title="Color", value="navy", options=COLORS)
-    input = TextInput(title="Number of points", value="200")
+def load_data():
+    data = pd.read_csv("data/PoliceKillingsUS.csv", encoding="ISO-8859-1")
+    data["year"] = data.date.apply(lambda x: int("20{}".format(x.split("/")[-1])))
+    return data
 
-    def update_color(attrname, old, new):
-        r.glyph.fill_color = select.value
-    select.on_change('value', update_color)
 
-    def update_points(attrname, old, new):
-        N = int(input.value)
-        source.data = get_data(N)
-    input.on_change('value', update_points)
+def prepare_data(data):
+    aggregated_data = data.groupby("state").agg("size")
+    states = aggregated_data.index.tolist()
+    counts = [x for x in aggregated_data]
+    return states, counts
 
-    layout = column(row(select, input, width=400), row(p))
 
-    doc.add_root(layout)
+data = load_data()
 
-show(modify_doc)
+states, counts = prepare_data(data)
+source = ColumnDataSource(data=dict(states=states, counts=counts))
+
+# input controls
+year = RangeSlider(title="Year", start=data.year.min(), end=data.year.max(), value=(data.year.min(), data.year.max()))
+age = RangeSlider(title="Age", start=data.age.min(), end=data.age.max(), value=(data.age.min(), data.age.max()))
+manner_of_death = Select(title="Manner of death", value="All", options=["All"] + data.manner_of_death.unique().tolist())
+armed = Select(title="Armed", value="All", options=["All"] + data.armed.dropna().unique().tolist())
+gender = Select(title="Gender", value="All", options=["All"] + data.gender.dropna().unique().tolist())
+race = Select(title="Race", value="All", options=["All"] + data.race.dropna().unique().tolist())
+threat_level = Select(title="Threat level", value="All", options=["All"] + data.threat_level.dropna().unique().tolist())
+flee = Select(title="Flee", value="All", options=["All"] + data.flee.dropna().unique().tolist())
+
+controls = [year, age, manner_of_death, armed, gender, race, threat_level, flee]
+sizing_mode = 'fixed'  # 'scale_width' also looks nice with this example
+inputs = widgetbox(*controls, sizing_mode=sizing_mode)
+
+p = figure(x_range=states, plot_height=350, plot_width=1000, toolbar_location=None, title="States counts")
+p.vbar(x='states', top='counts', width=0.9, source=source)
+
+# p = figure(plot_height=600, plot_width=700, title="", toolbar_location=None)
+l = layout([
+    [inputs, p],
+], sizing_mode=sizing_mode)
+
+curdoc().add_root(l)
